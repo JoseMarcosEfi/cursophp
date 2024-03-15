a68d028a3945c356937ef831704f2ee39cc6f262
Jose Marcos <thelegendchase2013@hotmail.com>
2024-03-15 10:31:41 -0300

    termino desafios1 php

--- desafios 01/desafio04/desafio4.php
+++ desafios 01/desafio04/desafio4.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
     <title>Desafio04</title>
 </head>
 <body>
     <main>
         <header><h1>Analisador de Número Real</h1></header>
         <?php 
-        $number = $_GET["number"];
-        print("Analisado o numero $number informado pelo usuário");
+        $number = $_POST["number"] ?? 0;
+        print("Analisado o numero $number informado pelo usuário ");
         $parts = explode(".", $number);
         if($parts[0] != null){
-            print("A parte inteira do número é $parts[0]");
+            print("A parte inteira do número é $parts[0] ");
         }else{
             print("Não existe parte inteira");
         }
@@ -22,10 +23,22 @@
         }else{
             print("Não existe parte decimal");
         }
+                
         
+        echo"<h1>Outra forma</h1>";
+                   
+        $num = $_POST["number"] ?? 0;
+    
+        echo "<p>Analisando o numero <strong> $num</strong> informado pelo usuário:</p>";
+
+        $int = (int) $num;
+        $fra = $num - $int;
+
+        echo "<ul><li> A parte inteira do número é <strong>". number_format($int, 0,",", ".")."</strong></li>";
+        echo "<li> A parte fracionária do número é <strong></strong>". number_format($fra, 3,",", ".")."</li></ul>";
         
         ?>
-
+        <button onclick="javascript:history.go(-1)">Voltar</button>
 
     </main>
     
