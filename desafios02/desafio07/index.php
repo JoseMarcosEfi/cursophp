62e8d73be943adf92125e4bb1f1d12803858704a
JoseMarcosEfi <72453138+SakakibaraKun@users.noreply.github.com>
2024-03-23 12:36:23 -0300

    desafio 2.7 concluido

--- desafios02/desafio07/index.php
+++ desafios02/desafio07/index.php
@@ -11,39 +11,41 @@
 <body>
     <?php
     $segundosForm = $_GET['segundos'];
-    $dias =0;
-    $horas=0;
-    $minutos=0;
-    $segundos=0;
-    $semana=0;
-    $aux=0;
-    
+    $dias = 0;
+    $horas = 0;
+    $minutos = 0;
+    $segundos = 0;
+    $semana = 0;
+    $aux = 0;
+
     //semana
-    if($segundosForm >=604800){
+    if ($segundosForm >= 604800) {
         $semana = floor($segundosForm / 604800);
         $aux = $segundosForm % 604800;
-    }else{
+    } else {
         $aux = $segundosForm;
     }
     //dias
-    if($aux >= 86400){
-        $dias = floor($aux /86400);
+    if ($aux >= 86400) {
+        $dias = floor($aux / 86400);
         $aux %= 86400;
     }
     //horas
-    if($aux >= 3600){
-        $horas = floor($aux /3600);
+    if ($aux >= 3600) {
+        $horas = floor($aux / 3600);
         $aux %= 3600;
     }
     //minutos
-    if($aux >= 60){
-        $minutos = floor($aux /60);
+    if ($aux >= 60) {
+        $minutos = floor($aux / 60);
         $aux %= 60;
-        
-    }else{
-        $aux = $segundosForm;
+
     }
-      
+    if ($aux < 60) {
+        $segundos = $aux;
+
+    }
+
 
     ?>
 
@@ -59,11 +61,21 @@
         <h1>Totalizando Tudo</h1>
         <?= "<p>Analisando o valor que vc digitou, <strong>$segundosForm segundos</strong> equivalem a um total de:</p>" ?>
         <ul>
-            <li><?= $semana ." Semanas"?></li>
-            <li><?= $dias. " Dias"?></li>
-            <li><?= $horas. " Horas"?></li>
-            <li><?= $minutos. " Minutos"?></li>
-            <li><?= $segundos. " Segundos"?></li>
+            <li>
+                <?= $semana . " Semanas" ?>
+            </li>
+            <li>
+                <?= $dias . " Dias" ?>
+            </li>
+            <li>
+                <?= $horas . " Horas" ?>
+            </li>
+            <li>
+                <?= $minutos . " Minutos" ?>
+            </li>
+            <li>
+                <?= $segundos . " Segundos" ?>
+            </li>
         </ul>
     </section>
 
